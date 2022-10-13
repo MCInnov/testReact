0eedd551d07e0681c55a2683e656dca7cb7a81d0
MCYnov <maximilien.costa13@gmail.com>
2022-10-13 13:41:19 +0200

    Header and Matchs

--- App.js
+++ App.js
@@ -1,75 +1,107 @@
-// import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar, Image } from 'react-native';
+
+// Images
 import pic_video from './assets/pictures/cleats-on-soccer-ball.jpg';
+import pic_team1 from './assets/pictures/capdail.webp';
+import pic_team2 from './assets/pictures/as fontonne foot.png';
+import pic_start from './assets/jouer.png';
 
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Open up App.js to start working on your app!</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
+// Composants
+import Header from './component/header.js';
 
-const DATA = [
-  {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'First Item',
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Second Item',
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
-  },
-];
 
 const testData = [
   {
-    subtitle: "Replay Match du 19/09/22 à 14h",
-    team1: {
-      picture: "",
-      name: "CAP D'AIL"
+    data: {
+      subtitle: "Replay Match du 19/09/22 à 14h",
+      team1: {
+        picture: pic_team1,
+        name: "CAP D'AIL"
+      },
+      team2: {
+        picture: pic_team2,
+        name: "ANTIBES"
+      },
+      sport: "FOOTBALL",
+      type: "REPLAY",
     },
-    team2: {
-      picture: "",
-      name: "ANTIBES"
+    test: "Test",
+    id: 0
+  },
+  {
+    data: {
+      subtitle: "Replay Match du 19/09/22 à 14h",
+      team1: {
+        picture: pic_team1,
+        name: "CAP D'AIL"
+      },
+      team2: {
+        picture: pic_team2,
+        name: "ANTIBES"
+      },
+      sport: "FOOTBALL",
+      type: "REPLAY",
     },
-    sport: "FOOTBALL",
-    type: "REPLAY",
-  }
+    test: "Test",
+    id: 1
+  },
+  {
+    data: {
+      subtitle: "Replay Match du 19/09/22 à 14h",
+      team1: {
+        picture: pic_team1,
+        name: "CAP D'AIL"
+      },
+      team2: {
+        picture: pic_team2,
+        name: "ANTIBES"
+      },
+      sport: "FOOTBALL",
+      type: "REPLAY",
+    },
+    test: "Test",
+    id: 2
+  },
 ];
 
-const Item = ({ title }) => (
-  <View style={styles.item}>
-    <Text style={styles.title}>{title}</Text>
-  </View>
-);
-
 const App = () => {
-  const renderItem = ({ item }) => (
-    <Item title={item.title} />
+  const renderMatch = ({ item }) => (
+    <View style={styles.match}>
+      <View style={styles.headerView}>
+        <Image source={item.data.team1.picture} style={styles.headerPic}></Image>
+        <Text style={styles.textTeam}>{item.data.team1.name}</Text>
+        <Text style={styles.textVS}>VS</Text>
+        <Text style={styles.textTeam}>{item.data.team2.name}</Text>
+        <Image source={item.data.team2.picture} style={styles.headerPic}></Image>
+      </View>
+      <View style={styles.playerView}>
+        <Text style={styles.sport}>{item.data.sport}</Text>
+        <Text style={styles.type}>{item.data.type}</Text>
+        <Image
+          style={[styles.player, styles.shadowProp]}
+          source={pic_video}
+        />
+        <Image
+          style={styles.startPlayer}
+          source={pic_start}
+        />
+      </View>
+      <Text style={styles.subtitle}>{item.data.subtitle}</Text>
+    </View>
   );
 
   return (
     <View style={styles.home}>
+      <Header></Header>
       <SafeAreaView style={styles.container}>
         <FlatList
-          data={DATA}
-          renderItem={renderItem}
+          data={testData}
+          renderItem={renderMatch}
           keyExtractor={item => item.id}
         />
       </SafeAreaView>
-      <View style={styles.test}>
-        <Text>Test</Text>
-        <Image
-          style={styles.tinyLogo}
-          source={pic_video}
-        />
-      </View>
-    </View>
+
+    </View >
 
   );
 }
@@ -85,9 +117,12 @@ const styles = StyleSheet.create({
     height: 250,
   },
   container: {
-    flex: 1,
+    flex: 4,
     paddingTop: StatusBar.currentHeight,
-    marginHorizontal: 16,
+    // marginHorizontal: 16,
+    marginTop: 70,
+    height: 300,
+
   },
   item: {
     // backgroundColor: "#f9c2ff",
@@ -101,12 +136,97 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24
   },
-  test: {
-    flex: 1,
+  match: {
+    flex: 2,
     flexDirection: "column",
-    alignContent: "center",
-    width: "100%"
-  }
+    alignItems: "center",
+    width: "100%",
+    marginTop: 15,
+    marginBottom: 15,
+  },
+
+  player: {
+    borderRadius: "11px",
+    width: 350,
+    height: 225,
+  },
+
+  headerView: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    alignContent: "space-around",
+    height: 200,
+    marginBottom: 10
+  },
+
+  headerPic: {
+    width: 50,
+    height: 50,
+    marginLeft: 10,
+    marginRight: 10
+  },
+
+  textTeam: {
+    marginLeft: 10,
+    marginRight: 10,
+    color: "#FFF",
+    fontWeight: 600,
+  },
+  textVS: {
+    marginLeft: 5,
+    marginRight: 5,
+    fontSize: 20,
+    fontWeight: 600,
+    color: "#FE0152",
+  },
+  subtitle: {
+    color: "#ADB4DC",
+    marginTop: 15
+  },
+  sport: {
+    top: 5,
+    left: 5,
+    zIndex: 100,
+    position: "absolute",
+    backgroundColor: "linear-gradient(202deg, rgba(255, 255, 255, 0.5) 0 %, rgba(216, 216, 216, 0.5) 76.82 %)",
+    paddingTop: 3,
+    paddingBottom: 3,
+    paddingLeft: 5,
+    paddingRight: 5,
+    borderRadius: "11px",
+    borderColor: "#FFF",
+    borderWidth: 1,
+    color: "#FFF"
+  },
+  type: {
+    position: "absolute",
+    top: 35,
+    left: -10,
+    zIndex: 100,
+    backgroundColor: "#FE0152",
+    paddingTop: 3,
+    paddingBottom: 3,
+    paddingLeft: 5,
+    paddingRight: 5,
+    borderRadius: "11px",
+    color: "#FFF"
+  },
+  shadowProp: {
+    shadowColor: '#171717',
+    shadowOffset: { width: -2, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+  startPlayer: {
+    zIndex: 100,
+    position: "absolute",
+    top: 75,
+    left: 150,
+    width: 50,
+    height: 50,
+  },
+
 });
 
 
